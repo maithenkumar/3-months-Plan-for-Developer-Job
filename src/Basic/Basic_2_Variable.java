d3d60ed408807be5a75fe3e73137e8be180559d6
Mona Lisaascascsacsac <maitheen1905@gmail.com>
2025-12-16 11:15:15 +0530

    first commit

--- src/Basic/Basic_2_Variable.java
+++ src/Basic/Basic_2_Variable.java
@@ -1,4 +1,12 @@
 package Basic;
 
 public class Basic_2_Variable {
+
+public static  void main(String [] args ){
+    String Name="Maithenkumar";
+    int age =23;
+    char gender ='M';
+    System.out.println("Employee Details of "+ Name + " \nMy age is  " + age +"\nGender is "+ gender + " - Male ");
+
+}
 }
